811de4e33d5ce85f2223370ad9d177b5d2f830b0
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-08 15:13:54 +0200

    tailwind

--- src/components/CountButton.tsx
+++ src/components/CountButton.tsx
@@ -1,11 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
 
-export default function CountButton() {
+type Props = {
+  className: string;
+};
+
+const CountButton = (_props: Props) => {
   const [count, setCount] = useState(0);
 
   return (
-    <button onClick={() => setCount((count) => count + 1)}>
+    <button
+      onClick={() => setCount((count) => count + 1)}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+    >
       count is {count}
     </button>
   );
-}
+};
+
+export default CountButton;
