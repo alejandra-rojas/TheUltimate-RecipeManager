6bc7ab9060c2af4b5ffdd89ff64dad2c18bca260
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-08 19:44:19 +0200

    feat: RecipeRepo

---
+++ Services/IRecipeRepository.cs
@@ -0,0 +1,12 @@
+using recipeManagerBackend.Models;
+namespace recipeManagerBackend.Services;
+
+public interface IRecipeRepository
+{
+    Recipe? GetOne(string id);
+    IEnumerable<Recipe> GetAll();
+    Recipe Create(string name, int servings, string ingredients, string instructions, string? source, string? image, string? comments);
+    void Delete(string id);
+    Recipe Update(string id, string name, int servings, string ingredients, string instructions, string? source, string? image, string? comments);
+
+}
---
+++ Services/RecipeRepository.cs
@@ -0,0 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using recipeManagerBackend.Models;
+
+namespace recipeManagerBackend.Services;
+
+public class RecipeRepository(AppDbContext context) : IRecipeRepository
+{
+
+    private readonly AppDbContext _context = context;
+    private DbSet<Recipe> _recipes => _context.Recipes;
+
+    public Recipe Create(string name, int servings, string ingredients, string instructions, string? source, string? image, string? comments)
+    {
+        var recipe = new Recipe
+        {
+            Name = name,
+            Servings = servings,
+            Ingredients = ingredients,
+            Instructions = instructions,
+            Source = source,
+            Image = image,
+            Comments = comments
+        };
+
+        _recipes.Add(recipe);
+        _context.SaveChanges();
+        return recipe;
+    }
+
+    public void Delete(string id)
+    {
+        var recipe = GetOne(id);
+        if (recipe is null)
+        {
+            return;
+        }
+        _recipes.Remove(recipe);
+        _context.SaveChanges();
+    }
+
+    public IEnumerable<Recipe> GetAll() => _recipes.AsEnumerable();
+
+    public Recipe? GetOne(string id) => _recipes.SingleOrDefault(c => c.Id.Equals(id));
+
+    public Recipe Update(string id, string name, int servings, string ingredients, string instructions, string? source, string? image, string? comments)
+    {
+        var foundRecipe = GetOne(id) ?? throw new KeyNotFoundException($"No recipe of ID {id} was found");
+
+        foundRecipe.Name = name;
+        foundRecipe.Servings = servings;
+        foundRecipe.Ingredients = ingredients;
+        foundRecipe.Instructions = instructions;
+        foundRecipe.Source = source;
+        foundRecipe.Image = image;
+        foundRecipe.Comments = comments;
+
+        _context.SaveChanges();
+        return foundRecipe;
+    }
+}
