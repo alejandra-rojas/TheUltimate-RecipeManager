5f5c5c739dcac9d09f6ca1817b23920753a54adb
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-08 21:00:08 +0200

    add: recipes component

---
+++ src/components/Recipes.tsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface Recipe {
+  id: string;
+  name: string;
+  servings: number;
+  ingredients: string;
+  instructions: string;
+  source: string;
+  image: string;
+  comments: string;
+}
+
+const url = "http://localhost:5273/api/Recipes";
+
+export default function Recipes() {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  //   useEffect(() => {
+  //     getRecipes();
+  //   }, []);
+  //   const getRecipes = () => {
+  //     axios.get<Recipe[]>(url).then((res) => setRecipes(res.data));
+  //   };
+
+  //   useEffect(() => {
+  //     fetch(url)
+  //       .then((results) => {
+  //         return results.json();
+  //       })
+  //       .then((data) => {
+  //         setRecipes(data);
+  //       });
+  //   }, []);
+
+  useEffect(() => {
+    const getRecipes = async () => {
+      try {
+        const recipes = await fetchRecipes(url);
+        setRecipes(recipes);
+      } catch (error) {
+        console.error("Error fetching recipes:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getRecipes();
+  }, []);
+
+  const fetchRecipes = async (url: string): Promise<Recipe[]> => {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  };
+
+  console.log(recipes);
+
+  if (loading) return <div>Loading...</div>;
+
+  return <div>Recipes</div>;
+}
