3ef3cedad206a078cf5ad95f0a9474b418d4fb2e
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-09 17:58:49 +0200

    add: text

--- frontend/src/components/filterbar.tsx
+++ frontend/src/components/filterbar.tsx
@@ -3,7 +3,7 @@ function filterbar() {
     <section className="bg-white shadow">
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <p className="font-bold tracking-tight text-gray-500">
-          Filters will go here
+          Filters and search-bar will go here
         </p>
       </div>
     </section>
