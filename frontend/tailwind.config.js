62654a1becb45a202f9a5128cbc7fd0f60cbfd45
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-09 15:59:24 +0200

    add: fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,13 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        azeret: ["Azeret Mono", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        robotomono: ["Roboto Mono", "sans-serif"],
+      },
+    },
   },
   plugins: [daisyui],
   daisyui: {
