cdeebc291672a36215fd4d7899ea992b97c18fdb
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-09 20:15:51 +0200

    refactor: file rename

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import Navbar from "./components/navbar";
 import FilterBar from "./components/filterbar";
-import RecipesGallery from "./components/recipes-gallery";
+import RecipesGallery from "./components/recipesGallery";
 
 function App() {
   return (
