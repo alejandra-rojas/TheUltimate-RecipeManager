82353ec24b61b889a5d3152bf0d0671e58f417f1
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-09 14:36:05 +0200

    add: fields

--- backend/Models/Recipe.cs
+++ backend/Models/Recipe.cs
@@ -6,6 +6,7 @@ public class Recipe
     [Key]
     public string Id { get; init; } = "recipe_" + Guid.NewGuid().ToString().Replace("-", "");
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public string? CreationSource { get; set; }
     public required string Name { get; set; }
     public required int Servings { get; set; }
     public required string Ingredients { get; set; }
@@ -13,4 +14,6 @@ public class Recipe
     public string? Source { get; set; }
     public string? Image { get; set; }
     public string? Comments { get; set; }
+    public int Rating { get; set; } = 0;
+
 }
