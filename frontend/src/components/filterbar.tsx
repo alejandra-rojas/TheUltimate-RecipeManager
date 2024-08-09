15b2bbb933f432d8c3e17e44af0f580fd6a73b6a
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-09 17:56:02 +0200

    add: basic styling for filter bar

--- frontend/src/components/filterbar.tsx
+++ frontend/src/components/filterbar.tsx
@@ -1,5 +1,13 @@
 function filterbar() {
-  return <div>The filter bar goes here</div>;
+  return (
+    <section className="bg-white shadow">
+      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <p className="font-bold tracking-tight text-gray-500">
+          Filters will go here
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default filterbar;
