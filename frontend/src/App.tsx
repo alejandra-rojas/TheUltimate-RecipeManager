f0d62d6ee7b5d0cb5e8c00f9badb72cec84e4e17
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-09 20:27:25 +0200

    refactor: lifting state to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,45 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useState } from "react";
 import Navbar from "./components/navbar";
 import FilterBar from "./components/filterbar";
 import RecipesGallery from "./components/recipesGallery";
 
+interface Recipe {
+  id: string;
+  name: string;
+  servings: number;
+  ingredients: string;
+  instructions: string;
+  source: string;
+  image: string;
+  comments: string;
+}
+
+const url = "http://localhost:5273/api/Recipes";
+
 function App() {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetch(url)
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        setRecipes(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("There was a problem while fetching the recipes:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  console.log(recipes);
+
+  if (loading) return <div>Loading...</div>;
+
   return (
     <>
       <Navbar />
--- frontend/src/components/recipesGallery.tsx
+++ frontend/src/components/recipesGallery.tsx
@@ -1,63 +1,6 @@
-import { useEffect, useState } from "react";
 import RecipeCard from "./recipeCard";
 
-interface Recipe {
-  id: string;
-  name: string;
-  servings: number;
-  ingredients: string;
-  instructions: string;
-  source: string;
-  image: string;
-  comments: string;
-}
-
-const url = "http://localhost:5273/api/Recipes";
-
 export default function Recipes() {
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  //   useEffect(() => {
-  //     fetch(url)
-  //       .then((results) => {
-  //         return results.json();
-  //       })
-  //       .then((data) => {
-  //         setRecipes(data);
-  //       });
-  //   }, []);
-
-  useEffect(() => {
-    const getRecipes = async () => {
-      try {
-        const recipes = await fetchRecipes(url);
-        setRecipes(recipes);
-      } catch (error) {
-        console.error("Error fetching recipes:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getRecipes();
-  }, []);
-
-  const fetchRecipes = async (url: string): Promise<Recipe[]> => {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error(`Error: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    return data;
-  };
-
-  console.log(recipes);
-
-  if (loading) return <div>Loading...</div>;
-
   return (
     <main className="mx-auto max-w-7xl flex flex-col flex-wrap gap-5 mt-8 px-4 py-6 sm:px-6  sm:flex-row sm:gap-6 lg:gap-6 lg:px-8">
       <RecipeCard />
