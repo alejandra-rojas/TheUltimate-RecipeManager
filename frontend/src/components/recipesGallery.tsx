719a45a7d8873e6f59249613e6531537e05d6e81
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-10 09:01:45 +0200

    add: mapping recipes to return card

--- frontend/src/components/recipesGallery.tsx
+++ frontend/src/components/recipesGallery.tsx
@@ -5,10 +5,23 @@ export default function Recipes({ recipes }: { recipes: Recipe[] }) {
   console.log("Props", recipes);
   return (
     <main className="mx-auto max-w-7xl flex flex-col flex-wrap gap-5 mt-8 px-4 py-6 sm:px-6  sm:flex-row sm:gap-6 lg:gap-6 lg:px-8">
-      <RecipeCard />
-      <RecipeCard />
-      <RecipeCard />
-      <RecipeCard />
+      {recipes.map((recipe) => {
+        return (
+          <RecipeCard
+            comments={recipe.comments}
+            createdAt={recipe.createdAt}
+            creationSource={recipe.creationSource}
+            id={recipe.id}
+            name={recipe.name}
+            servings={recipe.servings}
+            ingredients={recipe.ingredients}
+            instructions={recipe.instructions}
+            image={recipe.image}
+            source={recipe.source}
+            rating={recipe.rating}
+          />
+        );
+      })}
     </main>
   );
 }
