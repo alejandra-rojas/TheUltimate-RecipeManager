a4a14ac895f941d432a579138659e276f3693841
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-10 13:53:50 +0200

    new: addRecipe component

---
+++ frontend/src/components/addRecipeBtn.tsx
@@ -0,0 +1,10 @@
+export default function addRecipeBtn() {
+  return (
+    <a
+      className="inline-block rounded-lg border border-indigo-600 bg-indigo-600 px-5 py-2 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+      href="#"
+    >
+      Add Recipe
+    </a>
+  );
+}
