d31b5144c2b06b5dc39f1a50c9cb3e002b6ff859
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-10 14:39:45 +0200

    style: refactor

--- frontend/src/components/dietKeyIcon.tsx
+++ frontend/src/components/dietKeyIcon.tsx
@@ -1,7 +1,9 @@
 export default function dietKeyIcon(props: { children: React.ReactNode }) {
   return (
-    <div className="border-gray-600 border-solid border-[0.75px] rounded-full w-6 h-5 flex items-center justify-center">
-      <span className="text-gray-600 text-xs">{props.children}</span>
-    </div>
+    <>
+      <span className="whitespace-nowrap rounded-full border-gray-500 border-solid border-[0.75px] w-6.5 h-5 px-2 py-1  flex items-center justify-center text-xs text-gray-700 ">
+        {props.children}
+      </span>
+    </>
   );
 }
