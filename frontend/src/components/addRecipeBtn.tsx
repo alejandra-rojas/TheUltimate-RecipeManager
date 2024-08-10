0b421684dbbecbcfc58b0b6792790382e3669964
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-10 18:10:11 +0200

    refactor: styling

--- frontend/src/components/addRecipeBtn.tsx
+++ frontend/src/components/addRecipeBtn.tsx
@@ -1,7 +1,7 @@
 export default function addRecipeBtn() {
   return (
     <a
-      className="inline-block rounded-lg border border-indigo-600 bg-indigo-600 px-5 py-2 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+      className="inline-block rounded-lg border border-indigo-600 px-5 py-2 text-sm font-medium text-indigo-600 hover:bg-transparent hover:text-white hover:bg-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
       href="#"
     >
       Add Recipe
