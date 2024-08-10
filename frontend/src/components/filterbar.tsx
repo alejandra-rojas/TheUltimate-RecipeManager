c51620779f4f14d8439444818bf5965f283f5667
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-10 17:01:10 +0200

    styling: adding area for filters

--- frontend/src/components/filterbar.tsx
+++ frontend/src/components/filterbar.tsx
@@ -7,7 +7,8 @@ function filterbar({
 }) {
   return (
     <section className="bg-white shadow">
-      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex justify-between">
+        <p className="text-gray-400">other filters will go here</p>
         <div className="relative">
           <label htmlFor="Search" className="sr-only">
             Search
