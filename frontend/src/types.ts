3ad2237164f168e31ac923cb9e1903784b8102bc
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-10 09:01:10 +0200

    refactor: added fields

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,12 +1,13 @@
 export interface Recipe {
   id: string;
   createdAt: string;
+  creationSource: string | null;
   name: string;
   servings: number;
   ingredients: string;
   instructions: string;
-  source: string;
-  image: string;
-  comments: string;
+  source: string | null;
+  image: string | null;
+  comments: string | null;
   rating: number;
 }
