1aba2f46dcfed869a2c2f9778279f517806e2e78
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-08-12 11:23:15 +0200

    styling

--- frontend/src/components/dietKeyBadge.tsx
+++ frontend/src/components/dietKeyBadge.tsx
@@ -1,7 +1,7 @@
 export default function dietKeyBadge(props: { children: React.ReactNode }) {
   return (
     <>
-      <span className="flex items-center justify-center text-[0.55rem] text-black w-6.5 h-5 px-2 py-1 rounded-full border-gray-800 border-solid border-[0.5px] whitespace-nowrap">
+      <span className="flex items-center justify-center text-[0.55rem] text-gray-600 bg-gray-100 w-6.5 h-5 px-2 py-1 rounded-full  border-solid border-[1px] whitespace-nowrap">
         {props.children}
       </span>
     </>
